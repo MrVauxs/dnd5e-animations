b6292d2fae6189c0b918f4099720458ea83380f4
mrvauxs <mrvauxs@gmail.com>
2023-03-13 02:44:04 +0000

    add tests

---
+++ .github/check-autorec.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+fs.readFile("module/autorec.json", function (err, data) {
+    if (err) {
+        console.log("Error: " + err);
+        process.exit(1);
+    }
+    var json = JSON.parse(data);
+    if (json.length == 0) {
+        console.error("Error: autorec.json is empty?!");
+        process.exit(1);
+    }
+
+    let passed = true;
+    Object.keys(json).forEach(function (key) {
+        const array = json[key];
+        if (Array.isArray(array)) {
+            array.filter(function (item) {
+                if (item.metaData == null) {
+                    passed = false
+                    console.error("Error: autorec.json contains an entry without metaData: " + item.label + ", id: " + item.id);
+                } else if (item.metaData.name !== "5e Animations" || isNaN(item.metaData.version)) {
+                    passed = false
+                    console.error("Error: autorec.json contains an entry with wrong metaData: " + item.label + ", id: " + item.id);
+                }
+            });
+        }
+    });
+
+    if (passed) {
+        console.log("autorec.json is valid.")
+    }
+
+    process.exit(0);
+})
